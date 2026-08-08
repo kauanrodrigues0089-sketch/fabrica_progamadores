79679384350cebe447582502f4b81c1b5744ecb7
kauanrodrigues0089-sketch <kauanrodrigues0089@gmail.com>
2026-08-08 16:56:05 -0300

    minha primeira pagina web

---
+++ web.py
@@ -0,0 +1,12 @@
+# Autor:Kauan Pietro
+# Projeto:minha primeira pagina web
+
+# importando a biblioteca
+import streamlit as st
+
+st.write('ola mundo!!!')
+st.write('kauan pietro')
+st.write('---sistema do calculo de IMC...')
+st.text_input('digite seu peso: ')
+st.text_input('digite sua altura: ')
+st.button('calcular IMC')
