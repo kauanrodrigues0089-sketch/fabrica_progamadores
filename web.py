01d00fa60318b78bb290b39db1d5df843c5ec4f3
kauanrodrigues0089-sketch <kauanrodrigues0089@gmail.com>
2026-08-15 15:33:19 -0300

    python avancado web

--- web.py
+++ web.py
@@ -4,9 +4,24 @@
 # importando a biblioteca
 import streamlit as st
 
-st.write('ola mundo!!!')
-st.write('kauan pietro')
-st.write('---sistema do calculo de IMC...')
-st.text_input('digite seu peso: ')
-st.text_input('digite sua altura: ')
-st.button('calcular IMC')
+st.title('---sistema do calculo de IMC...')
+peso = st.number_input('digite seu peso: ')
+altura = st.number_input('digite sua altura: ')
+if st.button('calcular IMC'):
+    if peso > 0 and altura > 0:
+        imc = peso / (altura ** 2)
+        st.success(f'Seu IMC é: {imc}', icon="✅")
+        if imc <= 18.5:
+            st.error('Abaixo do peso', icon="🚨")
+        elif imc <= 24.9:
+            st.success('peso normal', icon="✅")
+        elif imc <= 29.9:
+            st.warning('Sobrepeso', icon="⚠️")
+        elif imc <= 34.9:
+            st.warning('Obesidade grau I', icon="⚠️")
+        elif imc <= 39.9:
+            st.warning('Obesidade grau II', icon="⚠️")
+        else:
+             st.error('Obesidade grau III (mórbida)', icon="🚨")
+    else:
+        st.error('Digite um número valído', icon="🚨")
